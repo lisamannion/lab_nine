d50d5b3c8a12f5fcd6c41509fee3a9bb4a310c23
lisamannion <61831482+lisamannion@users.noreply.github.com>
2021-05-16 19:06:23 +0100

    Lab nine complete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  myStatus:string;
 
-  constructor() {}
+  constructor(private storage:Storage) {}
+
+  ngOnInit(): void {
+    this.storage.get("status")
+    .then((data)=>{
+      this.myStatus = data;
+    })
+    .catch()
+  }
 
 }
---
+++ src/app/status/status.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-status',
+  templateUrl: './status.page.html',
+  styleUrls: ['./status.page.scss'],
+})
+export class StatusPage implements OnInit {
+  myStatus:string = "undefined";
+
+  constructor(private storage:Storage, private navCtrl:NavController) { }
+
+  ngOnInit() {
+    this.storage.get("status")
+    .then((data)=>{
+      this.myStatus=data;
+    })
+    .catch();
+  }
+
+  updateStatus(){
+    this.storage.set('status', this.myStatus)
+    .then(()=>{
+      this.navCtrl.navigateBack('/home');
+    })
+    .catch()
+  }
+
+}
